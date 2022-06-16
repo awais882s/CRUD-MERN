71feacfc21a50e7385d72c30e9e6157dd0826337
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-06-16 14:29:53 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+app.use(express.json());
+mongoose.connect("", {
+  useNewUrlParser: true,
+});
+
+app.listen(3001, () => {
+  console.log(`Server is running on port 3001...`);
+});
