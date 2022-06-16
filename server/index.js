9772fd1eee2f8ad1b0ffbb1da67f58f65f4cb647
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-06-16 15:25:26 +0500

    connect with mongo db

--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const FoodeModel = require("./models/Food");
 app.use(express.json());
-mongoose.connect("", {
-  useNewUrlParser: true,
+mongoose.connect(
+  "mongodb+srv://newuser:12345@crud.r7u0ft5.mongodb.net/food?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+  }
+);
+
+app.get("/", async (req, res) => {
+  const food = new FoodeModel({ foodName: "Apple", daysSinceIAte: 3 });
+  try {
+    await food.save();
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.listen(3001, () => {
